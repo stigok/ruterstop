06d3307d5a09669734deb29fae6587d60656db94
Stig Otnes Kolstad <stig@stigok.com>
2019-11-01 22:37:11 +0100

    chore: fiks klager fra pylint

--- ruterstop.py
+++ ruterstop.py
@@ -73,8 +73,8 @@ def human_delta(until=None, *, since=None):
 
     if mins < 1:
         return now_str
-    else:
-        return "{:2} min".format(mins)
+
+    return "{:2} min".format(mins)
 
 
 class Departure(namedtuple("Departure", ["line", "name", "eta", "direction"])):
@@ -158,7 +158,7 @@ def timed_cache(*, expires_sec=60, now=datetime.now):
         @functools.wraps(func)
         def wrapper(*_args, **_kwargs):
             time = now()
-            key = functools._make_key(_args, _kwargs, False)
+            key = functools._make_key(_args, _kwargs, False) # pylint: disable=protected-access
 
             if key not in cache or time > cache[key]["timestamp"] + timedelta(seconds=expires_sec):
                 cache[key] = dict(value=func(*_args, **_kwargs), timestamp=time)
