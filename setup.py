c9627072313a0ba19dca4027844b6c1f83730d5c
Stig Otnes Kolstad <stig@stigok.com>
2019-11-14 16:12:50 +0100

    feat: pakk sammen med setuptools
    mye enklere å bruke enn distutils.core, og gir bedre fleksibilitet
    for å distribuere et eksekverbart skript.

--- setup.py
+++ setup.py
@@ -1,7 +1,17 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='ruterstop',
     version='0.0.1',
-    scripts=['ruterstop.py',],
+    packages=["ruterstop"],
+    scripts=['bin/ruterstop',],
+    tests_require=[
+        "coverage",
+        "freezegun",
+    ],
+    install_requires=[
+        "bottle",
+        "requests",
+    ],
+    test_suite="ruterstop.tests"
 )
