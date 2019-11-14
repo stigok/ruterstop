706f7087998cdbb1ae33af076e2b67f1ca4b4693
Stig Otnes Kolstad <stig@stigok.com>
2019-11-14 16:13:09 +0100

    chore: legg til mer pakke-metadata

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ from setuptools import setup
 setup(
     name='ruterstop',
     version='0.0.1',
+    url="https://github.com/stigok/ruterstop",
+    author="stigok",
+    author_email="stig@stigok.com",
     packages=["ruterstop"],
     scripts=['bin/ruterstop',],
     tests_require=[
@@ -13,5 +16,5 @@ setup(
         "bottle",
         "requests",
     ],
-    test_suite="ruterstop.tests"
+    test_suite="ruterstop.tests",
 )
